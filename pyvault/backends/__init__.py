f7974716d7617e5f2c668d94f52448e93977683f
Tobias Heinzen <drtoful@users.noreply.github.com>
2014-06-29 11:00:06 +0200

    storage backend

---
+++ pyvault/backends/__init__.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+
+class PyVaultBackend(object):
+    def create(self):
+        pass
+
+    def get_meta(self):
+        pass
+
+    def retrieve(self, key):
+        pass
+
+    def store(self, key, data):
+        pass
---
+++ pyvault/backends/file.py
@@ -0,0 +1,52 @@
+#-*- coding: utf-8 -*-
+
+import os
+import json
+from backends import PyVaultBackend
+
+class PyVaultFileBackend(PyVaultBackend):
+    def __init__(self, path):
+        self._path = path
+
+    def create(self):
+        os.mkdirs(self._path, 0700)
+
+    def set_meta(self, data):
+        file = os.path.join(self._path, ".meta")
+
+        if os.path.isfile(file):
+            raise ValueError
+
+        with open(file, "w") as fp:
+            json.dump(fp)
+
+        os.chmod(file, 0600)
+
+    def get_meta(self):
+        file = os.path.join(self._path, ".meta")
+        if not os.path.isfile(file):
+            raise ValueError
+
+        with open(file, "r") as fp:
+            data = json.load(fp)
+
+        return data
+
+    def retrieve(self, key):
+        file = os.path.join(self._path, key)
+        if not os.path.isfile(file):
+            raise ValueError
+
+        with open(file, "r") as fp:
+            data = json.load(fp)
+
+        return data
+
+    def store(self, key, data):
+        file = os.path.join(self._path, key)
+
+        with open(file, "w") as fp:
+            data = json.dump(fp)
+
+        os.chmod(file, 0600)
+
