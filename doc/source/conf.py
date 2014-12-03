9e3685e0055dfd373f3fd2b76bc1c18a976a3d35
Tobias Heinzen <drtoful@users.noreply.github.com>
2014-12-03 21:35:36 +0100

    adding github fork to layout

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -99,7 +99,7 @@ html_theme = 'flasky'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'index_logo': False, 'github_fork': False}
+html_theme_options = {'index_logo': False, 'github_fork': "drtoful/pyvault"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['../theme']
