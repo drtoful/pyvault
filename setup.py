d432eaeecf9876f720de4fa9e3b83900544704cb
Tobias Heinzen <drtoful@users.noreply.github.com>
2014-12-06 16:27:07 +0100

    releasing 0.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyvault',
-    version='0.2.1',
+    version='0.2.2',
     packages=["pyvault", "pyvault.ciphers", "pyvault.backends"],
     install_requires=[
         'pycrypto >= 2.6',
